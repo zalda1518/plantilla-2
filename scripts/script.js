4fa3d8492915c8d6caf728357ef19c0bc5229871
zalda1518 <respaldos1992@gmail.com>
2025-09-01 21:56:26 -0500

    primer commit

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+// Parallax para montañas
+const back = document.getElementById("mountain-back");
+const front = document.getElementById("mountain-front");
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+ /*  back.style.transform = `translateY(${value * 0.2}px)`;   // más lento
+  front.style.transform = `translateY(${value * 0.5}px)`;  */ // más rápido
+});
+
+// Animación de imágenes al hacer scroll (reveal)
+const images = document.querySelectorAll('.content-block img');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.2 });
+
+images.forEach(img => observer.observe(img));
+
+
+/* ocultar header con scroll */
+
+let lastScrollTop = 0;
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // scroll down → ocultar
+    header.classList.add("hidden");
+  } else {
+    // scroll up → mostrar
+    header.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // evitar valores negativos
+});
